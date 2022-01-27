8809e774a69d38cc0c9da25403bd7a1a277a7c25
nnikhil <nikhil_narendran@hotmail.com>
2022-01-27 18:08:52 -0500

    created Thought and User models

---
+++ models/User.js
@@ -0,0 +1,53 @@
+const { Schema, model } = require("mongoose");
+const moment = require("moment");
+
+const UserSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      match: [
+        /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+        "Please enter an email address",
+      ],
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Thought",
+      },
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true,
+    },
+    id: false,
+  }
+);
+
+// create the User model using the UserSchema
+const User = model("User", UserSchema);
+
+// get total count of friends on retrieval
+UserSchema.virtual("friendCount").get(function () {
+  return this.friends.length;
+});
+
+// export the User model
+module.exports = User;
